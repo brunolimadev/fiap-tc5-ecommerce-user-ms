3bca86272322fdea1dd851a5b6cd5f2ad242f717
brunolimadev <bruno.lima.dev@outlook.com>
2024-06-20 20:31:10 -0300

    first commit

---
+++ src/main/java/br/com/fiap/fiap_tc5_ecommerce_user_ms/FiapTc5EcommerceUserMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiap_tc5_ecommerce_user_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapTc5EcommerceUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapTc5EcommerceUserMsApplication.class, args);
+	}
+
+}

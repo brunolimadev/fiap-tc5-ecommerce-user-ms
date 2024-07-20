4fc6d9aa8dba7200d50fe2f9726ea407f1dd03c5
Bruno <bruno.lima.dev@outlook.com>
2024-07-19 22:29:26 -0300

    fix: unit tests

--- src/test/java/br/com/fiap/fiap_tc5_ecommerce_user_ms/FiapTc5EcommerceUserMsApplicationTests.java
+++ src/test/java/br/com/fiap/fiap_tc5_ecommerce_user_ms/FiapTc5EcommerceUserMsApplicationTests.java
@@ -1,13 +1,8 @@
 package br.com.fiap.fiap_tc5_ecommerce_user_ms;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FiapTc5EcommerceUserMsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
